29a4c579710ed25437761f5c4ae3d49bd059c787
Ricardo Rodriguez <ricardo96r@gmail.com>
2019-09-14 22:20:31 +0200

    seed in one sql query

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
 require "faker"
 
+data = []
 100000.times do
-  Student.create([{
-          name: Faker::Name.name,
-          lastname: Faker::Name.last_name,
-          birthday: Faker::Date.backward(),
-          address: Faker::Address.full_address
-      }])
+  data.push({
+                name: Faker::Name.name,
+                lastname: Faker::Name.last_name,
+                birthday: Faker::Date.backward(),
+                address: Faker::Address.full_address
+            })
 end
+
+Student.create(data)
